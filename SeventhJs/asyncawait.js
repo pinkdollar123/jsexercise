72d3a1620d247b0172c0e2d239a74721fa810b31
ZorsCode <zhosyuy21@gmail.com>
2023-07-19 08:05:03 +0800

    added multiple switch case practices from freecodecamp basic javascript certification

--- SeventhJs/asyncawait.js
+++ SeventhJs/asyncawait.js
@@ -1,81 +1,96 @@
-"use strict";
+// "use strict";
 
 
 
-let oneFunction = arg => {
+// let oneFunction = arg => {
     
-    return new Promise(resolve => {
+//     return new Promise(resolve => {
         
-        setTimeout(() => {
-            var returnVal = arg;
-            console.log("oneFunction produces:", returnVal);
-            resolve(returnVal);
+//         setTimeout(() => {
+//             var returnVal = arg;
+//             console.log("oneFunction produces:", returnVal);
+//             resolve(returnVal);
 
-        },2000);
-    });
-};
+//         },2000);
+//     });
+// };
 
-let twoFunction = arg => {
-
-    return new Promise(resolve => {
-
-        setInterval(() => {
-
-            var returnVal = arg + "Two";
-            console.log("twoFunction produces", returnVal);
-            resolve(returnVal);
-        },2000)
-    });
-};
-
-let threeFunction = arg => {
-
-    return new Promise(resolve => {
-
-        setTimeout(() => {
-
-            var returnVal = arg + "Three";
-            console.log("threeFunction produces:", returnVal);
-            resolve(returnVal);
-        },2000);
-    });
-};
-
-// * we can implement this 3 function in one function using 
-// * new Promise resolve , rejected and then method it's Promise Chain
-// let finalval = arg => {
+// let twoFunction = arg => {
 
 //     return new Promise(resolve => {
 
-//         oneFunction(arg).then((val) => {
+//         setInterval(() => {
 
-//             twoFunction(val).then((val) => {
-
-//                 threeFunction(val).then((val) => {
-
-//                     console.log("Final val:", val);
-//                 })
-
-//             })
-//         })
+//             var returnVal = arg + "Two";
+//             console.log("twoFunction produces", returnVal);
+//             resolve(returnVal);
+//         },2000)
 //     });
+// };
+
+// let threeFunction = arg => {
+
+//     return new Promise(resolve => {
+
+//         setTimeout(() => {
+
+//             var returnVal = arg + "Three";
+//             console.log("threeFunction produces:", returnVal);
+//             resolve(returnVal);
+//         },2000);
+//     });
+// };
+
+// // * we can implement this 3 function in one function using 
+// // * new Promise resolve , rejected and then method it's Promise Chain
+// // let finalval = arg => {
+
+// //     return new Promise(resolve => {
+
+// //         oneFunction(arg).then((val) => {
+
+// //             twoFunction(val).then((val) => {
+
+// //                 threeFunction(val).then((val) => {
+
+// //                     console.log("Final val:", val);
+// //                 })
+
+// //             })
+// //         })
+// //     });
+// // }
+
+// // finalval("One");
+
+// // ? async and await syntax To do that, we need to define our finalValAsync() function as an asynchronous function by using the async keyword. Just as in the previous version, this access a single argument. And this is pass or fed to the first function, so the invocation of the firstFunction returns the requested value, and we used await keyword,
+
+// let finalValAsync = async arg => {
+
+//     // * Until the function returns the promise the code execution of finalValAsync() will be paused. And once the promise return, however the code execution can move on the next line where once again we will await the return of the promise from the secondFunction and continue to threeFunction before the console log message is invoked
+//     var one = await oneFunction(arg);
+//     var two = await twoFunction(one);
+//     var val = await threeFunction(two);
+
+//     console.log("Final val:", val);
 // }
 
-// finalval("One");
+// finalValAsync("one");
 
-// ? async and await syntax To do that, we need to define our finalValAsync() function as an asynchronous function by using the async keyword. Just as in the previous version, this access a single argument. And this is pass or fed to the first function, so the invocation of the firstFunction returns the requested value, and we used await keyword,
+// "use strict";
 
-let finalValAsync = async arg => {
+function Person(name, age, email)
+{
+    this.name=name;
+    this.age=age;
+    this.email=email;
 
-    // * Until the function returns the promise the code execution of finalValAsync() will be paused. And once the promise return, however the code execution can move on the next line where once again we will await the return of the promise from the secondFunction and continue to threeFunction before the console log message is invoked
-    var one = await oneFunction(arg);
-    var two = await twoFunction(one);
-    var val = await threeFunction(two);
-
-    console.log("Final val:", val);
+    this.Greetings = () =>
+{
+    console.log(`Hello my name is ${this.name} and I'm ${this.age} years old`);
+}
 }
 
-finalValAsync("one");
+const carlo = new Person("carlo", 14, "carlo@email.com");
 
-
-
+carlo.Greetings();
