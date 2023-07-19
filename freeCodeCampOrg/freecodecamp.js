0a3bc9fcc34d155a6143f3efaffa8ed3612a3cec
ZorsCode <zhosyuy21@gmail.com>
2023-07-19 11:36:32 +0800

    Added object manipulation and some conditional changes

--- freeCodeCampOrg/freecodecamp.js
+++ freeCodeCampOrg/freecodecamp.js
@@ -42,3 +42,46 @@ function chainToSwitch(val) {
   chainToSwitch(7);
   chainToSwitch("John");
   chainToSwitch(156);
+
+  // Setup
+function abTest(a, b) {
+    // Only change code below this line
+    if (a < 0 || b < 0){
+      return ;
+    }
+    // Only change code above this line
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  abTest(2,2);
+
+  let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
---
+++ freeCodeCampOrg/freecodecampobj.js
@@ -0,0 +1,156 @@
+"use strict";
+
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
+
+  // Setup
+const testObj2 = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj2["an entree"];   // Change this line
+  const drinkValue = testObj2["the drink"];    // Change this line
+
+  // Setup
+const testObj3 = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  const playerNumber = 16;  // Change this line
+  const player = testObj3[playerNumber];   // Change this line
+
+  // Setup
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.name = "Happy Coder";
+  myDog["bark"] = "woof";
+  delete myDog.tails;
+  // Only change code below this line
+
+  function phoneticLookup(val) {
+    var result = "";
+    var lookup = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+    };
+  // After converting our case statements into object properties you can make use of the variable `result` to let the function return the correct value.
+  
+  
+    result = lookup[val];
+    // Only change code above this line
+    return result;
+  }
+  
+
+  function checkObj(obj, checkProp) {
+    // Only change code below this line
+   let checker = obj.hasOwnProperty(checkProp);
+    if(checker){
+      return obj[checkProp];
+      // Code above calls the object and display the given value from checkProp
+    }else {
+      return "Not Found";
+    }
+    // Only change code above this line
+  }
+
+//   Flexible Data Structure
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      artist: "Kamote",
+      title: "Sinigang",
+      release_year: 2023,
+      formats: ["Digital Copy", "1GB", "HP"]
+    }
+  ];
+
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
+
+  // Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+
+  if(value === ""){
+    delete records[id][prop];
+  }
+  else if(prop !== "tracks" && value !== ""){
+    records[id][prop] = value;
+  }
+  else if(prop === "tracks" && value !== ""){
+   if(records[id].hasOwnProperty("tracks") === false){
+     records[id][prop] = [];
+   }
+   records[id][prop].push(value);
+  }
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
