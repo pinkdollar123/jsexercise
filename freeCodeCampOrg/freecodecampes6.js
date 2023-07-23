1e8b6b2cdbb1a52428bb863c66c51b0a99ae7c7e
ZorsCode <zhosyuy21@gmail.com>
2023-07-23 18:42:39 +0800

    studied closure scope, block scode, global scope, mutating const array, Object.freeze(), arrow function and array concat()

--- freeCodeCampOrg/freecodecampes6.js
+++ freeCodeCampOrg/freecodecampes6.js
@@ -55,3 +55,15 @@ function freezeObj() {
     return MATH_CONSTANTS.PI;
   }
   const PI = freezeObj();
+
+  const magic = () => { return new Date();}
+
+//   Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+  };
+  
+  console.log(myConcat([1, 2], [3, 4, 5]));
+  
+  
