45d6a8e9303d5d48c70c742fed7364a7498df509
ZorsCode <zhosyuy21@gmail.com>
2023-07-24 15:25:38 +0800

    added array spread operator trial

--- freeCodeCampOrg/freecodecampes6.js
+++ freeCodeCampOrg/freecodecampes6.js
@@ -79,3 +79,12 @@ const sum = (...args) => {
 // Only change code below this line
 const increment = (number, value = 1) => number + value;
 // Only change code above this line
+
+// copying the spread operator in an array1 to array 2
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
