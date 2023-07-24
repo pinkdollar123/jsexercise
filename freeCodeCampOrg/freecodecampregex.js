d558c942e94b39e58b30b82706ca7b762c8d6f9e
ZorsCode <zhosyuy21@gmail.com>
2023-07-25 07:23:40 +0800

    started learning regex from freeCodeCamp understood the test method comparing the first variable and using the test method and passing the second variable inside the close parenthesis as argument

---
+++ freeCodeCampOrg/freecodecampregex.js
@@ -0,0 +1,10 @@
+// ? If you want to find the word the in the string The dog chased the cat, you could use the following regular expression: /the/. Notice that quote marks are not required within the regular expression.
+
+// ? JavaScript has multiple ways to use regexes. One way to test a regex is using the .test() method. The .test() method takes the regex, applies it to a string (which is placed inside the parentheses), and returns true or false if your pattern finds something or not.
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+myRegex.test(myString) // Change this line
+
+console.log(myRegex);
+
