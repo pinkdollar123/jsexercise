598bf2074df0c1572c0f82922b98d99c424296da
ZorsCode <zhosyuy21@gmail.com>
2023-07-24 15:51:19 +0800

    removing first to element in an array and returning the rest of the array after declaring it to a variable called list and not invoking the first two parameter

--- freeCodeCampOrg/freecodecampes6.js
+++ freeCodeCampOrg/freecodecampes6.js
@@ -137,3 +137,15 @@ let a = 8, b = 6;
 
 const [d, e,,, f] = [1, 2, 3, 4, 5, 6];
 console.log(d, e, f);
+
+// Use a destructuring assignment with the rest syntax to emulate the behavior of Array.prototype.slice(). removeFirstTwo() should return a sub-array of the original array list with the first two elements omitted. 
+
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [a, b, ...shorterList] = list // Change this line
+  // Only change code above this line
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
