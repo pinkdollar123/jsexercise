5b0b9e38ae08ec8a3c850f85d2550ed6066ec174
ZorsCode <zhosyuy21@gmail.com>
2023-07-24 16:20:35 +0800

    template literal using backtick   and iterating the array value and array index to an array container and pushing it to the array container

--- freeCodeCampOrg/freecodecampes6.js
+++ freeCodeCampOrg/freecodecampes6.js
@@ -163,3 +163,23 @@ const stats = {
 // Only change code below this line
 const half = ({max,min}) => (max + min) / 2.0; 
 // Only change code above this line
+
+// template literal using backtick ` ` and iterating the array value and array index to an array container and pushing it to the array container
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for(let i= 0; i< arr.length; i++){
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
