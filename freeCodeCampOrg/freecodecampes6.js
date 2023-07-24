545ef2cfa0b03aa108c7a32698b72b8dd37d0d52
ZorsCode <zhosyuy21@gmail.com>
2023-07-24 16:12:59 +0800

    destructuring properties of an object stats

--- freeCodeCampOrg/freecodecampes6.js
+++ freeCodeCampOrg/freecodecampes6.js
@@ -149,3 +149,17 @@ function removeFirstTwo(list) {
 
 const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+// destructuring properties of an object stats
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({max,min}) => (max + min) / 2.0; 
+// Only change code above this line
