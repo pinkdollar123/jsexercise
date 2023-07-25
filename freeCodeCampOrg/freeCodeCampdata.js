e4a6bb381f1abb249d3b1fc105321a036778ee4b
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 07:46:09 +0800

    arr.splice() returns a new array and also modifies the existing array

--- freeCodeCampOrg/freeCodeCampdata.js
+++ freeCodeCampOrg/freeCodeCampdata.js
@@ -24,4 +24,13 @@ function popShift(arr) {
   
   console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
-  
+//   * splice returns a new array and also modifies the original array
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(0,2);
+arr.splice(1,2);
+arr.splice(2,2);
+// Only change code above this line
+console.log(arr);
+
