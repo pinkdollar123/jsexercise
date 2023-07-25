9f07f98a0da9c4462021980f23eee76b9a5251b9
ZorsCode <zhosyuy21@gmail.com>
2023-07-25 08:16:03 +0800

    added character classes locator using  /[string to be located ]/

--- freeCodeCampOrg/freecodecampregex.js
+++ freeCodeCampOrg/freecodecampregex.js
@@ -57,3 +57,24 @@ let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/; // Change this line
 let result6 = unRegex.test(exampleStr);
 console.log(result6);
+
+// * You can search for a literal pattern with some flexibility with character classes. Character classes allow you to define a group of characters you wish to match by placing them inside square ([ and ]) brackets.
+
+// *For example, you want to match bag, big, and bug but not bog. You can create the regex /b[aiu]g/ to do this. The [aiu] is the character class that will only match the characters a, i, or u.
+
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+bigStr.match(bgRegex);
+bagStr.match(bgRegex);
+bugStr.match(bgRegex);
+bogStr.match(bgRegex);
+
+// ? Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample.
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result7 = quoteSample.match(vowelRegex); // Change this line
+
