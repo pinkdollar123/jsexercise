2ee7955295b005297c16c52a98db9bb10d392d08
ZorsCode <zhosyuy21@gmail.com>
2023-07-25 13:40:34 +0800

    added feature to find the specific only count number of the characters using {} quantity specifiers curly braces {value,}

--- freeCodeCampOrg/freecodecampregex.js
+++ freeCodeCampOrg/freecodecampregex.js
@@ -230,6 +230,45 @@ let spaceRegex = /\s/g;
 whiteSpace.match(spaceRegex);
 
 
+// * Search for non-whitespace using \S, which is an uppercase s. This pattern will not match whitespace, carriage return, tab, form feed, and new line characters. You can think of it being similar to the character class [^ \r\t\f\n\v].
+
+let whiteSpace2 = "Whitespace. Whitespace everywhere!"
+let nonSpaceRegex = /\S/g;
+whiteSpace2.match(nonSpaceRegex).length;
+
+// Specify Upper and Lower Number of Matches
+
+// Recall that you use the plus sign + to look for one or more characters and the asterisk * to look for zero or more characters. These are convenient but sometimes you want to match a certain range of patterns.
+
+// You can specify the lower and upper number of patterns with quantity specifiers. Quantity specifiers are used with curly brackets ({ and }). You put two numbers between the curly brackets - for the lower and upper number of patterns.
+
+// For example, to match only the letter a appearing between 3 and 5 times in the string ah, your regex would be /a{3,5}h/.
+
+let A4 = "aaaah";
+let A2 = "aah";
+let multipleA = /a{3,5}h/;
+multipleA.test(A4);
+multipleA.test(A2);
+
+// * with space locator
+
+let ohStr = "Ohhh no";
+let ohRegex = /oh{3,6}\sno/i; // Change this line
+let resultspace = ohRegex.test(ohStr);
+
+// * Specify Only the Lower Number of Matches You can specify the lower and upper number of patterns with quantity specifiers using curly brackets. Sometimes you only want to specify the lower number of patterns with no upper limit.
+
+// ? To only specify the lower number of patterns, keep the first number followed by a comma. For example, to match only the string hah with the letter a appearing at least 3 times, your regex would be /ha{3,}h/.
+
+A4 = "haaaah";
+A2 = "haah";
+let A100 = "h" + "a".repeat(100) + "h";
+multipleA = /ha{3,}h/;
+multipleA.test(A4);
+multipleA.test(A2);
+multipleA.test(A100);
 
 
-
+let haStr = "Hazzzzah";
+let haRegex = /haz{4,}ah/i; // Change this line added i flag to enable ignorecase for the string
+let resulthaz = haRegex.test(haStr);
