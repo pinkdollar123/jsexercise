d073d0f3e29956c2be39f8c1a866a9ef4f196df5
ZorsCode <zhosyuy21@gmail.com>
2023-07-25 16:09:41 +0800

    added whitespace checker and remove using regex

--- freeCodeCampOrg/freecodecampregex.js
+++ freeCodeCampOrg/freecodecampregex.js
@@ -353,3 +353,26 @@ let newresult2 = newmyRegex2.test(newmyString); // Change this line
 let repeatRegex = /(\w+) \1 \1/;
 repeatRegex.test(repeatStr); // Returns true
 repeatStr.match(repeatRegex); // Returns ["row row row", "row"]
+
+
+// * Use Capture Groups to Search and Replace ou can search and replace text in a string using .replace() on a string. The inputs for .replace() is first the regex pattern you want to search for. The second parameter is the string to replace the match or a function to do something.
+
+let wrongText = "The sky is silver.";
+let silverRegex = /silver/;
+wrongText.replace(silverRegex, "blue");
+
+
+let cc = "Code Camp".replace(/(\w+)\s(\w+)/, '$2 $1');
+
+// !Write a regex fixRegex using three capture groups that will search for each word in the string one two three. Then update the replaceText variable to replace one two three with the string three two one and assign the result to the result variable. Make sure you are utilizing capture groups in the replacement string using the dollar sign ($) syntax.
+
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let resultfix = str.replace(fixRegex, replaceText);
+
+// *Remove Whitespace from Start and End Write a regex and use the appropriate string methods to remove whitespace at the beginning and end of strings.
+
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let resulthello = hello.replace(wsRegex, ""); // Change this line
