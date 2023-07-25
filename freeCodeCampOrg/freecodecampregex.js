51f998adad4b85a37c9a92c3b3ed4c06768f902f
ZorsCode <zhosyuy21@gmail.com>
2023-07-25 13:18:15 +0800

    added feature to check whitespaces in a string using regex  /\s/g flag

--- freeCodeCampOrg/freecodecampregex.js
+++ freeCodeCampOrg/freecodecampregex.js
@@ -223,6 +223,12 @@ $ - end of input
 i - ignore case of input
 */
 
+// * You can search for whitespace using \s, which is a lowercase s. This pattern not only matches whitespace, but also carriage return, tab, form feed, and new line characters. You can think of it as similar to the character class [ \r\t\f\n\v].
+
+let whiteSpace = "Whitespace. Whitespace everywhere!"
+let spaceRegex = /\s/g;
+whiteSpace.match(spaceRegex);
+
 
 
 
