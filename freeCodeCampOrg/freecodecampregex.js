2f54d7795f433d763d844a08583ea86c718f0245
ZorsCode <zhosyuy21@gmail.com>
2023-07-25 12:56:35 +0800

    added feature to find digit and non-digit numbers using \d for digit, and \D for non digit and appending with global g flag

--- freeCodeCampOrg/freecodecampregex.js
+++ freeCodeCampOrg/freecodecampregex.js
@@ -167,5 +167,35 @@ shortHand.test(varNames);
 numbers.match(shortHand);
 sentence.match(shortHand);
 
+// * Matching all digit characters using \d This is equal to the character class [0-9], which looks for a single character of any number between zero and nine.
+
+// Your regex should find 1 digit in the string 9.
+// Waiting: Your regex should find 2 digits in the string Catch 22.
+// Waiting: Your regex should find 3 digits in the string 101 Dalmatians.
+// Waiting: Your regex should find no digits in the string One, Two, Three.
+// Waiting: Your regex should find 2 digits in the string 21 Jump Street.
+// Waiting: Your regex should find 4 digits in the string 2001: A Space Odyssey.
+
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let resultdigit = movieName.match(numRegex).length;
+
+
+// * Matching all non digit characters using \D This is equal to the character class [^0-9], which looks for a single character that is not a number between zero and nine.
+
+// Waiting: Your regex should find no non-digits in the string 9.
+// Waiting: Your regex should find 6 non-digits in the string Catch 22.
+// Waiting: Your regex should find 11 non-digits in the string 101 Dalmatians.
+// Waiting: Your regex should find 15 non-digits in the string One, Two, Three.
+// Waiting: Your regex should find 12 non-digits in the string 21 Jump Street.
+// Waiting: Your regex should find 17 non-digits in the string 2001: A Space Odyssey.
+
+let movieNamenon = "2001: A Space Odyssey";
+let noNumRegex = /\D/g; // Change this line
+let resultnon = movieNamenon.match(noNumRegex).length;
+
+
+
+
 
 
