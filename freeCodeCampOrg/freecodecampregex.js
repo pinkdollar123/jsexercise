06c1fccc376f57c7a90d81b460f5288e88977a53
ZorsCode <zhosyuy21@gmail.com>
2023-07-25 12:26:40 +0800

    added regex * start or asterisk character to find characters that occurs zero or more times

--- freeCodeCampOrg/freecodecampregex.js
+++ freeCodeCampOrg/freecodecampregex.js
@@ -111,6 +111,13 @@ let difficultSpelling = "Mississippi";
 let myRegexnew = /s+/g; // Change this line
 let resultnew = difficultSpelling.match(myRegex);
 
+// * regex finding characters that occurs zero or more times The character to do this is the asterisk or star: *.
 
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result9 = chewieQuote.match(chewieRegex);
 
 
