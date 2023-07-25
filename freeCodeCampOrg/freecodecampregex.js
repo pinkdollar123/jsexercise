c0a8e113a89d20ff24af30e0fb27b9855285f59a
ZorsCode <zhosyuy21@gmail.com>
2023-07-25 13:09:12 +0800

    added feature to check the usernames in a database to ensure the user can only follow the given restrictions

--- freeCodeCampOrg/freecodecampregex.js
+++ freeCodeCampOrg/freecodecampregex.js
@@ -194,6 +194,34 @@ let movieNamenon = "2001: A Space Odyssey";
 let noNumRegex = /\D/g; // Change this line
 let resultnon = movieNamenon.match(noNumRegex).length;
 
+// *You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+
+//   *  Usernames can only use alpha-numeric characters.
+
+//    * The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+
+//    * Username letters can be lowercase and uppercase.
+
+//    * Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+
+
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i; // Change this line
+let userResult = userCheck.test(username);
+
+
+/*
+Code Explanation
+^ - start of input
+[a-z] - first character is a letter 
+[0-9]{2,} - ends with two or more numbers
+| - or
+[a-z]+ - has one or more letters next
+\d* - and ends with zero or more numbers
+$ - end of input
+i - ignore case of input
+*/
 
 
 
