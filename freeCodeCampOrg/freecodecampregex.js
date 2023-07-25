e6dc9de27731888c4fce97c26f667680a4cc059c
ZorsCode <zhosyuy21@gmail.com>
2023-07-25 13:46:50 +0800

    added feature using question mark? after the optional string character such as color or colour in to colou?r

--- freeCodeCampOrg/freecodecampregex.js
+++ freeCodeCampOrg/freecodecampregex.js
@@ -283,5 +283,18 @@ let timStr = "Timmmmber";
 let timRegex = /Tim{4}ber/; // Change this line
 let resulttim = timRegex.test(timStr);
 
-// * Check for All or None
+// * Check for All or None You can specify the possible existence of an element with a question mark, ?. This checks for zero or one of the preceding element. You can think of this symbol as saying the previous element is optional.
+
+let american = "color";
+let british = "colour";
+let rainbowRegex= /colou?r/;
+rainbowRegex.test(american);
+rainbowRegex.test(british);
+
+// !Change the regex favRegex to match both the American English (favorite) and the British English (favourite) version of the word.
+
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let resultfav = favRegex.test(favWord);
+
 
