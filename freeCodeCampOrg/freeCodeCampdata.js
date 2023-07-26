48120a087ab31ea8cbad08e8bc47d0adad21a07c
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 13:51:39 +0800

    add Object.keys(objectname) method accepting an object as argument passed to the method

--- freeCodeCampOrg/freeCodeCampdata.js
+++ freeCodeCampOrg/freeCodeCampdata.js
@@ -258,3 +258,33 @@ function countOnline(allUsers) {
 
 console.log(countOnline(usersNew));
 
+// We can also generate an array which contains all the keys stored in an object with the Object.keys() method. This method takes an object as the argument and returns an array of strings representing each property in the object.
+
+let usersObject = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // Only change code below this line
+  let container = Object.keys(obj);
+  return container;
+  // Only change code above this line
+}
+
+console.log(getArrayOfUsers(usersObject));
+
