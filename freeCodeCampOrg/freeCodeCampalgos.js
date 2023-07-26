982190d1c9df10732aded7aa3fcd2740fc3ba88b
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 14:55:50 +0800

    added find longest word

--- freeCodeCampOrg/freeCodeCampalgos.js
+++ freeCodeCampOrg/freeCodeCampalgos.js
@@ -33,5 +33,18 @@ function factorialize(num) {
   
   factorialize(5);
 
+// Find the Longest Word in a String
 
+function findLongestWordLength(str) {
+    let words = str.split(' ');
+    let maxLength = 0;
+  
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > maxLength) {
+        maxLength = words[i].length;
+      }
+    }
+  
+    return maxLength;
+  }
   
