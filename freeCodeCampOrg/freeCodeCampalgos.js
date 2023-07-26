6b8e6df3e7d6292297c9632f635f9ee497c64cdc
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 14:51:47 +0800

    added factorialization

--- freeCodeCampOrg/freeCodeCampalgos.js
+++ freeCodeCampOrg/freeCodeCampalgos.js
@@ -23,5 +23,15 @@ function reverseString(str) {
   
 reverseString("hello");
 
+// factorializa using recursion
+function factorialize(num) {
+    if (num === 0) {
+      return 1;
+    }
+    return num * factorialize(num - 1);
+  }
+  
+  factorialize(5);
+
 
   
