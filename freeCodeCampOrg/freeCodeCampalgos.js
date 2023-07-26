ab41cad592d10ce0b1f1370ff08d4a5959b6dad6
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 15:29:36 +0800

    added different algos find the largest number in an array

--- freeCodeCampOrg/freeCodeCampalgos.js
+++ freeCodeCampOrg/freeCodeCampalgos.js
@@ -48,3 +48,41 @@ function findLongestWordLength(str) {
     return maxLength;
   }
   
+
+//   Find the largest numbers in arrays
+
+// * Procedural approach
+function largestOfFour(arr) {
+    const results = [];
+    for (let i = 0; i < arr.length; i++) {
+      let largestNumber = arr[i][0];
+      for (let j = 1; j < arr[i].length; j++) {
+        if (arr[i][j] > largestNumber) {
+          largestNumber = arr[i][j];
+        }
+      }
+      results[i] = largestNumber;
+    }
+  
+    return results;
+  }
+
+//   Code Explanation freecodecamp community
+
+//     Create a variable to store the results as an array.
+//     Create an outer loop to iterate through the outer array.
+//     Create a second variable to hold the largest number and initialise it with the first number. This must be outside an inner loop so it won’t be reassigned until we find a larger number.
+//     Create said inner loop to work with the sub-arrays.
+//     Check if the element of the sub array is larger than the currently stored largest number. If so, then update the number in the variable.
+//     After the inner loop, save the largest number in the corresponding position inside of the results array.
+//     And finally return said array.
+
+
+//   *Declarative approach
+function largestOfFour(arr) {
+    return arr.map(function(group) {
+      return group.reduce(function(prev, current) {
+        return current > prev ? current : prev;
+      });
+    });
+  }
