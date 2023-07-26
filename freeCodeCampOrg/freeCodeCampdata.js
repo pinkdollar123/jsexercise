940f73a5247055fb161804b17cf6eb4143865b89
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 08:14:52 +0800

    copy machine using array spread operator and using push to push the number of array from while loop and using push() to insert all the array elements.

--- freeCodeCampOrg/freeCodeCampdata.js
+++ freeCodeCampOrg/freeCodeCampdata.js
@@ -59,7 +59,22 @@ function forecast(arr) {
   // Only change code above this line
   console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 
+// * copy machine using array spread operator and using push to push the number of array from while loop and using push() to insert all the array elements.
+
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+      // Only change code below this line
+        newArr.push([...arr])
+      // Only change code above this line
+      num--;
+    }
+    return newArr;
+  }
   
+  console.log(copyMachine([true, false, true], 2));
+
+
 
 
 
