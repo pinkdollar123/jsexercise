a4d0701d79e983f44aedc8dfb3586477d3a0f286
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 12:09:30 +0800

    added nested array in 5 levels

--- freeCodeCampOrg/freeCodeCampdata.js
+++ freeCodeCampOrg/freeCodeCampdata.js
@@ -111,6 +111,17 @@ function filteredArray(arr, elem) {
 
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
 
+// * Nested array in 5 levels
+
+let myNestedArray = [
+  // Only change code below this line
+  'level 1',                   /* myNestedArray[0]             */
+  ['level 2'],	               /* myNestedArray[1][0]          */
+  [['level 3','deep']],	       /* myNestedArray[2][0][0]       */
+  [[['level 4','deeper']]],    /* myNestedArray[3][0][0][0]    */
+  [[[['level 5','deepest']]]]  /* myNestedArray[4][0][0][0][0] */
+  // Only change code above this line
+];
 
 
 
