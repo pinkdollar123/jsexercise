5d017a05168c8d1a13fda3995c7045d76ce1415e
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 12:39:03 +0800

    deleting object using delete objectname.propertyname or objectname['propertyname']

--- freeCodeCampOrg/freeCodeCampdata.js
+++ freeCodeCampOrg/freeCodeCampdata.js
@@ -176,4 +176,23 @@ function checkInventory(scannedItem) {
 
 console.log(checkInventory("apples"));
 
+// * deleting foods using delete object
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// Only change code below this line
+delete foods['oranges'];
+delete foods['plums'];
+delete foods['strawberries'];
+// Only change code above this line
+
+console.log(foods);
+
 
