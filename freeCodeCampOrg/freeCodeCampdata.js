987640698fdba30a695d6fa5344b633d7f837aa0
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 12:31:54 +0800

    added object.property modification in an inner depth

--- freeCodeCampOrg/freeCodeCampdata.js
+++ freeCodeCampOrg/freeCodeCampdata.js
@@ -139,8 +139,22 @@ foods.strawberries = 27;
 
 console.log(foods);
 
+// *object accessing and modifying existing property values
 
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
 
+// Only change code below this line
+userActivity.data.online = 45;
+// Only change code above this line
+
+console.log(userActivity);
 
 
 
