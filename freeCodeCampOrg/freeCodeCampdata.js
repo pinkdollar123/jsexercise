9e3ee3b0f2703a43aad190cfc2157b3d4dac322b
ZorsCode <zhosyuy21@gmail.com>
2023-07-26 12:31:23 +0800

    added object.property = value initialization

--- freeCodeCampOrg/freeCodeCampdata.js
+++ freeCodeCampOrg/freeCodeCampdata.js
@@ -123,6 +123,23 @@ let myNestedArray = [
   // Only change code above this line
 ];
 
+// *object adding property objectname.propertyname = values;
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// Only change code below this line
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+// Only change code above this line
+
+console.log(foods);
+
+
 
 
 
