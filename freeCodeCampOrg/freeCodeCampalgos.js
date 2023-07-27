3c7b2a1eca2da78ec8e37c0821cfaed41c87c685
ZorsCode <zhosyuy21@gmail.com>
2023-07-28 07:18:00 +0800

    added string mutation of an array

--- freeCodeCampOrg/freeCodeCampalgos.js
+++ freeCodeCampOrg/freeCodeCampalgos.js
@@ -331,7 +331,20 @@ function truncateString(str, num) {
   
   matcherme(3.5);
   
+  // String mutation 
+  function mutation(arr) {
+    let test = arr[1].toLowerCase();
+    let target = arr[0].toLowerCase();
+   for(let i = 0; i < test.length; i++){
+     if(target.indexOf(test[i]) < 0) return false;
+   }
+    return true;
+  }
   
+  mutation(["hello", "hey"]);
+  
+  let text = ["hello"];
+  console.log(text.splice(0));
   
 
   
