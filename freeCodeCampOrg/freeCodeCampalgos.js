6091f592090a14eea5b15fc73f31fb9b987c9472
ZorsCode <zhosyuy21@gmail.com>
2023-07-28 07:19:03 +0800

    added different ways to mutate a string inside an array

--- freeCodeCampOrg/freeCodeCampalgos.js
+++ freeCodeCampOrg/freeCodeCampalgos.js
@@ -347,5 +347,32 @@ function truncateString(str, num) {
   console.log(text.splice(0));
   
 
-  
-  
+  // * Declarative approach
+
+  function mutation(arr) {
+    return arr[1]
+      .toLowerCase()
+      .split("")
+      .every(function(letter) {
+        return arr[0].toLowerCase().indexOf(letter) !== -1;
+      });
+  }
+
+  // * Ternary
+
+  function mutation([ target, test ], i = 0) {
+    target = target.toLowerCase();
+    test = test.toLowerCase();
+    return i >= test.length
+      ? true
+      : !target.includes(test[i])
+        ? false
+        : mutation([ target, test ], i + 1);
+  }
+
+  // Regex 
+
+  function mutation([elem1, elem2]) {
+    const regex = new RegExp(`[^${elem1}]`, 'i');
+    return !regex.test(elem2);
+  }
