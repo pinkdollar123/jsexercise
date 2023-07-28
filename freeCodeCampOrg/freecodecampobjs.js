5b9523299e16bc1df3949d18c0a09b057863d39f
ZorsCode <zhosyuy21@gmail.com>
2023-07-28 10:02:02 +0800

    understand how IIFE works

--- freeCodeCampOrg/freecodecampobjs.js
+++ freeCodeCampOrg/freecodecampobjs.js
@@ -336,3 +336,11 @@ function Bird() {
       return weight;
     }
   }
+
+  // * Understand the Immediately Invoked Function Expression (IIFE)
+
+(function () {
+    console.log("A cozy nest is ready");
+  })();
+  
+  // ? Note that the function has no name and is not stored in a variable. The two parentheses () at the end of the function expression cause it to be immediately executed or invoked. This pattern is known as an immediately invoked function expression or IIFE.
