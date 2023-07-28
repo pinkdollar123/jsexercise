592b6d76f2cddb775d93927f52356d42f96229bc
ZorsCode <zhosyuy21@gmail.com>
2023-07-28 09:06:56 +0800

    constructor property in objects matching if the constructor was an instance of created class

--- freeCodeCampOrg/freecodecampobjs.js
+++ freeCodeCampOrg/freecodecampobjs.js
@@ -37,5 +37,54 @@ function Bird(name) {
   
   console.log(ownProps);
 
+//  * use prototyping Objectname.prototype.propertyname = value;
+
+function Dog(name) {
+    this.name = name;
+  }
   
+  Dog.prototype.numLegs = 4;
+  
+  // Only change code above this line
+  let beagle = new Dog("Snoopy");
+
+//   property checker using forloop and hasOwnProperty method
+
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  let beagle = new Dog("Snoopy");
+  
+  let ownProps = [];
+  let prototypeProps = [];
+  
+  // Only change code below this line
+  
+  for(let props in beagle){
+    if(beagle.hasOwnProperty(props)){
+      ownProps.push(props);
+    }else{
+      prototypeProps.push(props);
+    }
+  }
+
+//   Understanding constructor property
+
+function Dog(name) {
+    this.name = name;
+  }
+  
+  // Only change code below this line
+  function joinDogFraternity(candidate) {
+    if(candidate.constructor === Dog){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
 
