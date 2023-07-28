91e8e58f64c69b7e5f77e25fedd10012513c6fb3
ZorsCode <zhosyuy21@gmail.com>
2023-07-28 09:43:08 +0800

    added overriding inherited methods from the parent object

--- freeCodeCampOrg/freecodecampobjs.js
+++ freeCodeCampOrg/freecodecampobjs.js
@@ -269,3 +269,37 @@ let beagle = new Dog();
 
 beagle.eat();
 beagle.bark();
+
+// *Override Inherited Methods syntax :
+// ? ChildObject.prototype = Object.create(ParentObject.prototype);
+
+// !If you have an instance let duck = new Bird(); and you call duck.eat(), this is how JavaScript looks for the method on the prototype chain of duck:
+
+// duck => Is eat() defined here? No.
+// Bird => Is eat() defined here? => Yes. Execute it and stop searching.
+// Animal => eat() is also defined, but JavaScript stopped searching before reaching this level.
+// Object => JavaScript stopped searching before reaching this level.
+
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+
+Penguin.prototype.fly = () => {
+  return `Alas, this is a flightless bird.`;
+}
+
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
+
+
+
