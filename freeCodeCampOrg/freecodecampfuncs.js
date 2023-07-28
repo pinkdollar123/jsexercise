48d81ee94772b6429ba9785ea763f844f9a4928e
ZorsCode <zhosyuy21@gmail.com>
2023-07-28 22:16:00 +0800

    function declarative approach

--- freeCodeCampOrg/freecodecampfuncs.js
+++ freeCodeCampOrg/freecodecampfuncs.js
@@ -20,3 +20,66 @@ const getTea = (numOfCups) => {
 const tea4TeamFCC = getTea(40);
 // Only change code above this line
 
+// * Function that acecpts function as argument called higher orders
+
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+
+
+// * Avoid Mutations and Side Effects Using Functional Programming
+
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+  // block scope variable
+  let newVal = fixedValue + 1;
+  return newVal;
+
+  // Only change code above this line
+}
+
+
+// The global variable
+let fixedValue3 = 4;
+
+// Only change code below this line
+// variable was passed to the function
+function incrementer(x) {
+
+  return x + 1;
+
+  // Only change code above this line
+}
+
+incrementer(fixedValue3);
+
