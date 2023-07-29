1377f3a3b3d50a09a247010a0b0982d407c3b35b
ZorsCode <zhosyuy21@gmail.com>
2023-07-30 06:22:25 +0800

    object mapping filter, for loop using Object.keys to match the given argument

--- freeCodeCampOrg/FCCInterAlgo.js
+++ freeCodeCampOrg/FCCInterAlgo.js
@@ -50,3 +50,64 @@ function destroyer(arr) {
   }
   
   destroyer([1, 2, 3, 1, 2, 3], 2, 3);g
+
+  // * object mapping filter, for loop using Object.keys to match the given argument
+
+  function whatIsInAName(collection, source) {
+
+    const sourceKeys = Object.keys(source);
+    // console.log(sourceKeys);
+  
+    return collection.filter(obj => {
+      for(let i = 0 ; i < sourceKeys.length; i++){
+        // console.log(i)
+        // console.log(obj);
+        // console.log(obj[sourceKeys[i]]);
+        // console.log(source[sourceKeys[i]])
+        // console.log(source[sourceKeys[i]]);
+        if(obj[sourceKeys[i]] !== source[sourceKeys[i]]){
+          return false;
+        }
+      }
+      return true;
+    })
+  }
+  
+  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+  
+  // Define the Dog class
+  class Dog {
+    constructor(name, breed, age) {
+      this.name = name;
+      this.breed = breed;
+      this.age = age;
+    }
+  
+    bark() {
+      console.log('Woof! Woof!');
+    }
+  }
+  
+  // Create an instance of the Dog class
+  const myDog = new Dog('Max', 'Labrador Retriever', 3);
+  
+  let key = Object.keys({name: 'Max', breed: "Labrador Retriever"});
+  let keySource = {name: 'Max', breed: "Labrador Retriever"};
+  
+  console.log(key[0] === 'name');
+  console.log(key[1]);
+  console.log(keySource[key[1]] === myDog['breed']);
+  console.log(keySource[key[1]])
+  console.log(myDog['breed']);
+  console.log(myDog[key[0]]);
+  console.log(keySource[key[0]] === "Max");
+  // Access and log the properties of the Dog instance
+  console.log(`Name: ${myDog.name}`);
+  console.log(`Breed: ${myDog.breed}`);
+  console.log(`Age: ${myDog.age}`);
+  
+  // Call the bark method of the Dog instance
+  myDog.bark();
+  
+
+  
