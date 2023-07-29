073c57e67067b74e5607bb8934547cb299613aba
ZorsCode <zhosyuy21@gmail.com>
2023-07-29 12:34:48 +0800

    added learning for sorting ascending order and aalpha reverse alpha

--- freeCodeCampOrg/freecodecampfuncs.js
+++ freeCodeCampOrg/freecodecampfuncs.js
@@ -387,9 +387,42 @@ const squareList = (arr) => {
   // Only change code below this line
   return arr
           .filter(props => props > 0 && props % parseInt(props) === 0)
+          // used math operation to apply the square root of filtered numbers
           .map(props => Math.pow(props, 2));
   // Only change code above this line
 };
 
 const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
 console.log(squaredIntegers);
+
+// * Sort an Array Alphabetically using the sort Method
+
+function ascendingOrder(arr) {
+  return arr.sort(function(a, b) {
+    return a - b;
+  });
+}
+
+ascendingOrder([1, 5, 2, 3, 4]);
+
+// reversal order
+
+function reverseAlpha(arr) {
+  return arr.sort( (a, b) => {
+    return a === b ? 0 : a < b ? 1 : -1;
+  });
+}
+
+reverseAlpha(['l', 'h', 'z', 'b', 's']);
+
+function alphabeticalOrder(arr) {
+  // Only change code below this line
+    
+  return arr.sort((a, b) => {
+    return a === b ? 0 : a > b ? 1 : -1;
+  })
+  // Only change code above this line
+}
+
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+
