e67c782c75a01702a0d146863ac04cb0e01260d9
ZorsCode <zhosyuy21@gmail.com>
2023-08-03 08:15:48 +0800

    studied string to binary then binary to letter

--- freeCodeCampOrg/FCCInterAlgo.js
+++ freeCodeCampOrg/FCCInterAlgo.js
@@ -429,3 +429,33 @@ function steamrollArrayFor(val,flatArr=[]) {
   });
   return flatArr;
 }
+
+// Binary Agents
+
+function binaryAgent(str) {
+
+  // splits the string to array with space,
+  var biString = str.split(" ");
+
+  // storage for the united string from binary conversion
+  var uniString = [];
+  
+  console.log(biString);
+  /*using the radix (or base) parameter in parseInt, we can convert the binary
+      number to a decimal number while simultaneously converting to a char*/
+
+// loops through all the array length of the given string
+  for (var i = 0; i < biString.length; i++) {
+
+    // pushed the converted string to the array holder using String() method fromCharCode() method accepting value of the parseInt(string, 2);
+    uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
+  }
+
+  // we then simply join the string
+  return uniString.join("");
+}
+
+// test here
+binaryAgent(
+  "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+);
