59e7c1c9f4900f4188b79199d8f1088007f576ca
ZorsCode <zhosyuy21@gmail.com>
2023-08-03 11:20:01 +0800

    studied the ceasar cipher using a const of alphabet and executing a conditional to follow the format of ceasar cipher

---
+++ freeCodeCampOrg/FCCProject/fccceasarcipher.js
@@ -0,0 +1,33 @@
+function rot13(str) {
+
+    // declares a variable container for the alphabet
+      const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  
+      // storage for the decoded string
+      let decodedStr = '';
+  
+  // loops through the length of given str
+    for (let i = 0; i < str.length; i++){
+      
+      // assigns char to the str index
+      const char = str[i];
+  
+      // assigns the charIndex to the indexOf value from the alphabet as reference
+      const charIndex = alphabet.indexOf(char);
+  
+  // given conditional
+      if (charIndex === -1){
+        // if non alphabetic leave unchanged
+        decodedStr += char;
+      }else{
+        const decodedCharIndex = (charIndex + 13) % 26;
+        decodedStr += alphabet[decodedCharIndex];
+      }
+    }
+    return decodedStr;
+  }
+  
+  rot13("SERR PBQR PNZC");
+  
+  
+  
